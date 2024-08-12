cd8bec41022e0d1ada582f9b7f2899a221fff918
nikvoen <nikvoen@yandex.ru>
2024-08-12 04:46:39 +0300

    changes the wrong orange to the right one

--- src/components/pages/Events/widgets/Button/index.tsx
+++ src/components/pages/Events/widgets/Button/index.tsx
@@ -1,10 +1,12 @@
 import Props from "./Button.props";
 import {FC} from "react";
 import {AppTextTheme} from "../../theme";
+import '../../events.scss'
+
 
 function _getClassNames({className = '', filled = false}: { className?: string, filled?: boolean }): string {
     const filledClass = (
-        'tw-border-red-700 md:tw-bg-red-700 ' +
+        'tw-border-red-700 md:tw-bg-[#C13100] ' +
         'tw-border ' +
         'tw-rounded-[60px] ' +
         'tw-h-8 lg:tw-h-12 xl:tw-h-16 ' +
