e5eadbd266040f50f403296f8d87b47811e8f631
Nikita Voennyy <90532299+nikvoen@users.noreply.github.com>
2024-08-13 03:07:17 +0300

    Update Partners.tsx

--- src/components/pages/Partners/Partners.tsx
+++ src/components/pages/Partners/Partners.tsx
@@ -34,7 +34,7 @@ export const PartnersPage = () => {
                 <div key={index}>
                     <Link to={item.link}>
                         <div className={"partner-card"}>
-                            <img src={`https://img.freepik.com/free-photo/the-adorable-illustration-of-kittens-playing-in-the-forest-generative-ai_260559-483.jpg?size=338&ext=jpg&ga=GA1.1.2008272138.1723420800&semt=ais_hybrid`}/>
+                            <img src={${AppConfig.apiUri}/${item.photo}}/>
                         </div>
                     </Link>
                 </div>
