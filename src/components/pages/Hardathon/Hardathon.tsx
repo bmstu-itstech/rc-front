02b2a1b1cd3307f592c9d4eda60c3cb99c6d0bf4
Matvey <68477540+MathewPr@users.noreply.github.com>
2024-08-31 00:58:09 +0300

    rs-fix-25 (#26)
    * rs-fix-25
    
    * rs-fix-14
    
    * rs-fix-25-img

--- src/components/pages/Hardathon/Hardathon.tsx
+++ src/components/pages/Hardathon/Hardathon.tsx
@@ -1,72 +1,37 @@
 import "./hardathon.scss"
 import Logo from "../../utils/logo/Logo"
-import bizikov from "../../assets/images/bizikov.png"
-import React, {useEffect, useState} from "react";
-import axios from "axios";
-import {Link} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
+import {Hardathon} from "../../../domain/entities/hardathon";
+import {hardathonElement} from "../../../shared/apis/hardathon";
+import { useQuery } from '@tanstack/react-query';
 import {AppConfig} from "../../../core";
+import bizikov from "../../assets/images/bizikov.png"
+import React from "react";
 
-interface Hardathon {
-    title: string,
-    photo: string
-}
 
 export const HardathonPage = () => {
+    const { id } = useParams<{ id: string }>();
 
-    const [hardathons, setHardathons] = useState<Hardathon[]>([]);
-
-    useEffect(() => {
-        axios.get(`${AppConfig.apiUri}/api/v0/hardatons/?page=1`)
-            .then(res => {
-                setHardathons(res.data.hardatons);
-            }).catch(err => {
-                console.log(err);
-        })
-    }, []);
-
-    const slideRight = () => {
-        const board = document.querySelector('.board');
-        if (board) {
-            const firstSlide = board.firstElementChild;
-            const lastSlide = board.lastElementChild;
-            if (firstSlide) {
-                board.removeChild(firstSlide);
-                board.appendChild(firstSlide);
-            }
-        }
-    }
-
-    const slideLeft = () => {
-        const board = document.querySelector('.board');
-        if (board) {
-            const firstSlide = board.firstElementChild;
-            const lastSlide = board.lastElementChild;
-            if (lastSlide) {
-                board.removeChild(lastSlide);
-                board.insertBefore(lastSlide, firstSlide);
-            }
-        }
-    }
-
+    const { data: hardathon} = useQuery<Hardathon>({
+        queryKey: ['new-list', id],
+        queryFn: () => hardathonElement(id!)
+    });
     return (
         <section className={"page events-page"}>
-            <Logo title="Хардатон 2024"/>
+            <Logo title={hardathon?.title}/>
             <div className={"board-outer d-flex justify-content-center"}>
                 <div className="board">
-                    <div className={"bizikov-pic"}>
-                        <div className={"d-flex justify-content-center"}>
-                            <img src={bizikov} alt=""/>
-                        </div>
-
+                    <div className={"main-pic-container"}>
+                        <img className={"main-pic"} src={`${AppConfig.apiUri}${hardathon?.main_organizer_photo}`} alt=""/>
                     </div>
                     <div className={"box-hardathon d-inline-block"}>
                         <div className={"quote"}>
-                            <p>Давно полюбившийся формат робототехнических соревнований, где в течение нескольких дней финала участники разрабатывают робототехнический проект на определённую тему, а затем командам предстоит защитить свою работу.</p>
+                            <p>{hardathon?.main_organizer_word}</p>
                             <p className={"author"}>— главный организатор хардатона,
                                 Валерий Бизиков</p>
                         </div>
                         <div className={"buttons"}>
-                            <Link to={"/hardathons.ts.ts/1/details"}>
+                            <Link to={"details"}>
                                 <button className={"event-btn w-400"}>
                                     <p className="fw-bolder fs-4 text-uppercase text-light m-0">подробнее</p>
                                 </button>
