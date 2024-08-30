54f0d6bbf719987eacac149aacb17b182760250b
Matvey <68477540+MathewPr@users.noreply.github.com>
2024-08-30 11:24:10 +0300

    fix-rs-front-29/30

--- src/components/pages/Contacts/Contacts.tsx
+++ src/components/pages/Contacts/Contacts.tsx
@@ -1,7 +1,6 @@
 import "./contacts.scss"
 import Logo from "../../utils/logo/Logo"
-import main_director_photo from "../../assets/images/contacts/main_director.jpg"
-import diagonal_arrow from "../../assets/icons/diagonal_arrow.svg"
+import balakalo from "../../assets/images/contacts/balakalo.jpg"
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
 import React from "react";
@@ -13,10 +12,10 @@ export const ContactsPage = () => {
     const items = [
         <div className={"card mx-auto w-100 my-auto"}>
             <div className="contact-photo">
-                <img src={main_director_photo} className={""} alt="ЦРМ_main_director" />
+                <img src={balakalo} className={""} alt="ЦРМ_main_director" />
             </div>
             <div>
-                <p className={"text-scale-large"}>Бизиков Валерий</p>
+                <p className={"text-scale-large"}>Балакало Максим</p>
                 <p className={"text-scale-normal"}>Руководитель ЦМР</p>
             </div>
         </div>,
@@ -25,8 +24,8 @@ export const ContactsPage = () => {
         <div className={"card col-4 px-4 py-3 mx-auto w-100 my-auto"}>
             <div className="row row-cols-1 links information-card  text-start h-100 text-start">
                 <div className="col d-flex flex-wrap ">
-                    <span className="text-weight-normal text-scale-normal">Телефон: <a className="text-weight-normal text-scale-normal more-arrow" href="tel:+78005553535">8(800)555-35-35</a></span>
-                    <span className="text-weight-normal text-scale-normal">Email: <a className="text-weight-normal text-scale-normal more-arrow" href="mailto:cmr@bmstu.ru">cmr@bmstu.ru</a></span>
+                    <span className="text-weight-normal text-scale-normal">Телефон: <a className="text-weight-normal text-scale-normal more-arrow" href="tel:+79065689673">8(906)568-96-73</a></span>
+                    <span className="text-weight-normal text-scale-normal">Email: <a className="text-weight-normal text-scale-normal more-arrow" href="mailto:maks.balakalo@bmstu.ru">maks.balakalo@bmstu.ru</a></span>
                 </div>
                 <a className="col  bg-transparent border-0 d-flex more-arrow text-weight-normal text-scale-normal flex-wrap w-100">Политика перс. данных</a>
                 <a className="col  bg-transparent border-0 d-flex more-arrow text-weight-normal text-scale-normal flex-wrap w-100">Сайт Университета</a>
@@ -51,25 +50,25 @@ export const ContactsPage = () => {
                 <div className="desktop h-100  w-100">
                     <div className="container-fluid d-flex flex-row wrapper justify-content-center h-100 align-items-center my-0">
                         <div className="card col-4 orange-overlay">
-                            <img src={main_director_photo} className={"contact-photo"} alt="ЦРМ_director_photo" />
+                            <img src={balakalo} className={"contact-photo"} alt="ЦРМ_director_photo" />
                             <div className={"card-img-overlay d-flex flex-column justify-content-end align-items-center"}>
-                                <p className={"text-weight-normal text-scale-large"}>Бизиков Валерий</p>
+                                <p className={"text-weight-normal text-scale-large"}>Балакало Максим</p>
                                 <p className={"text-weight-bold text-scale-normal"}>Руководитель ЦМР</p>
-                                <p className={"text-weight-normal text-scale-normal"}>+7 (999) 999-99-99</p>
-                                <p className={"text-weight-normal text-scale-normal"}>1234567@mail.ru</p>
+                                <p className={"text-weight-normal text-scale-normal"}>+7 (906) 568-96-73</p>
+                                <p className={"text-weight-normal text-scale-normal"}>maks.balakalo@bmstu.ru</p>
                             </div>
                         </div>
                         <div className={"card col-4 "}>
                             <div className="row row-cols-1 links information-card  text-start h-100">
                                 <div className="col d-flex flex-wrap ">
-                                    <span className="text-scale-normal">Телефон: <a className="text-weight-normal text-scale-normal" href="tel:+78005553535">8(800)555-35-35</a></span>
+                                    <span className="text-scale-normal">Телефон: <a className="text-weight-normal text-scale-normal" href="tel:+78005553535">8(906)568-96-73</a></span>
                                     <span className="text-scale-normal">Email: <a className="text-weight-normal text-scale-normal" href="mailto:cmr@bmstu.ru">cmr@bmstu.ru</a></span>
                                 </div>
-                                <a className="col d-flex more-arrow  text-weight-normal text-scale-normal w-auto flex-wrap">Политика перс. данных</a>
-                                <a className="col d-flex more-arrow  text-weight-normal text-scale-normal w-auto flex-wrap">Сайт Университета</a>
+                                <a className="col d-flex more-arrow  text-weight-normal text-scale-normal w-auto flex-wrap" href={"https://bmstu.ru/about/obrabotka-dannyh"}>Политика перс. данных</a>
+                                <a className="col d-flex more-arrow  text-weight-normal text-scale-normal w-auto flex-wrap" href={"https://bmstu.ru/"}>Сайт Университета</a>
                                 <div className="col">
-                                    <span className={"text-weight-normal text-scale-normal d-flex gap-1 "}>Адрес: <a href="" className={"d-flex flex-nowrap more-arrow"}>Схема проезда</a> </span>
-                                    <p className="text-weight-normal text-scale-normal">105005, г. Москва, 2-ая Бауманская, д.5, стр.1</p>
+                                    <span className={"text-weight-normal text-scale-normal d-flex gap-1 "}>Адрес: <a href="https://yandex.ru/maps/-/CDcPvD9e" className={"d-flex flex-nowrap more-arrow"}>Схема проезда</a> </span>
+                                    <p className="text-weight-normal text-scale-normal">105005, г. Москва, Бауманская ул., 57Б, стр. 1</p>
                                 </div>
                                 <div className="col">
                                     <span className="text-weight-normal text-scale-normal">Соцсети:</span>
