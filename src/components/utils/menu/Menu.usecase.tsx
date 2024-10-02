fd2be6ee7012048fe5bf05ad962fce11def1608f
Robocotik <alexander.starkin@gmail.com>
2024-10-02 19:16:32 +0300

    feat(components/menu): fix fonts

---
+++ src/components/utils/menu/Menu.usecase.tsx
@@ -0,0 +1,20 @@
+const menuDefault = [
+  {
+    label: 'главная',
+    href: '/',
+  },
+  {
+    label: 'мероприятия',
+    href: '/events',
+  },
+  {
+    label: 'хардатоны',
+    href: '/hardathons',
+  },
+  {
+    label: 'партнёры',
+    href: '/partners',
+  },
+];
+
+export default menuDefault;
